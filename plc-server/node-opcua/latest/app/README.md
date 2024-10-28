a773949ddeffbdc84580de04a986cd5ca874b892
DocDriven <sebastian.kraust@gmail.com>
2024-10-28 12:02:27 +0100

    Add plc-server/node-opcua/latest image

---
+++ plc-server/node-opcua/latest/app/README.md
@@ -0,0 +1,14 @@
+# How to run the typescript code
+
+1. Install Node.js and npm
+2. Install packages
+```bash
+npm install
+```
+3. Run the code
+```bash
+npm start
+```
+
+The code will be transpiled to JavaScript into the `out` folder and then
+executed with the start script.
---
+++ plc-server/node-opcua/latest/pki/gen_kc_pair.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+KEY_LEN=2048
+DAYS_VALID=365
+CONFIG_FILE="/pki/ca.cnf"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Missing ca.cnf file at $CONFIG_FILE"
+  exit 1
+fi
+
+# Generate the RSA key
+openssl genrsa -out /pki/key.pem $KEY_LEN
+
+# Convert the key to DER format
+openssl pkey -inform PEM -outform DER -in key.pem -out key.der
+
+# Generate the certificate
+openssl req -x509 -new -key /pki/key.pem -out /pki/cert.pem -outform PEM \
+        -config "$CONFIG_FILE" -days $DAYS_VALID \
+        -addext "subjectAltName = URI:urn:opcua-server.local,IP:127.0.0.1,DNS:$HOSTNAME"
